f1b23282086d39b3a7d4158564fcc97b055c6f58
kokomi-0v0 <112543538+kokomi-0v0@users.noreply.github.com>
2022-10-04 15:49:26 +0800

    Add files via upload
    nebula

---
+++ precomputation-common/src/main/java/org/hnu/precomputation/common/model/api/CommonResult.java
@@ -0,0 +1,103 @@
+package org.hnu.precomputation.common.model.api;
+
+/**
+ * 通用返回对象封装
+ */
+public class CommonResult<T> {
+    /**
+     * 状态码
+     */
+    private Integer code;
+    /**
+     * 提示信息
+     */
+    private String message;
+    /**
+     * 数据封装
+     */
+    private T data;
+
+    protected CommonResult() {
+    }
+
+    protected CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data 获取的数据
+     */
+    public static <T> CommonResult<T> success(T data) {
+        return new CommonResult<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data 获取的数据
+     * @param  message 提示信息
+     */
+    public static <T> CommonResult<T> success(T data, String message) {
+        return new CommonResult<T>(ResultCode.SUCCESS.getCode(), message, data);
+    }
+
+    /**
+     * 失败返回结果
+     * @param errorCode 错误码
+     */
+    public static <T> CommonResult<T> failed(IErrorCode errorCode) {
+        return new CommonResult<T>(errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param errorCode 错误码
+     * @param message 错误信息
+     */
+    public static <T> CommonResult<T> failed(IErrorCode errorCode,String message) {
+        return new CommonResult<T>(errorCode.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param message 提示信息
+     */
+    public static <T> CommonResult<T> failed(String message) {
+        return new CommonResult<T>(ResultCode.FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     */
+    public static <T> CommonResult<T> failed() {
+        return failed(ResultCode.FAILED);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ precomputation-service/src/main/java/org/hnu/precomputation/service/service/NebulaGraphService.java
@@ -0,0 +1,236 @@
+package org.hnu.precomputation.service.service;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.vesoft.nebula.client.graph.NebulaPoolConfig;
+import com.vesoft.nebula.client.graph.data.HostAddress;
+import com.vesoft.nebula.client.graph.data.ResultSet;
+import com.vesoft.nebula.client.graph.data.ValueWrapper;
+import com.vesoft.nebula.client.graph.exception.IOErrorException;
+import com.vesoft.nebula.client.graph.net.NebulaPool;
+import com.vesoft.nebula.client.graph.net.Session;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.hnu.precomputation.common.model.Nebula.ClassAutoMapping;
+import org.hnu.precomputation.common.model.Nebula.FieldAutoMapping;
+import org.hnu.precomputation.common.model.api.NebulaResult;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.hnu.precomputation.common.model.api.NebulaConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.hnu.precomputation.common.model.Nebula.Edge;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.*;
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class NebulaGraphService {
+    private final static Logger logger = LoggerFactory.getLogger("NebulaGraphService");
+    /*初始化*/
+    public class NebulaConfig {
+        @Bean
+
+            public NebulaPool nebulaPool () throws Exception {
+                NebulaPool pool = new NebulaPool();
+                NebulaPoolConfig nebulaPoolConfig = new NebulaPoolConfig();
+                nebulaPoolConfig.setMaxConnSize(1000);
+                List<HostAddress> addresses = Arrays.asList(new HostAddress("127.0.0.1", 9669));
+                boolean init = pool.init(addresses, nebulaPoolConfig);
+                if (!init) {
+                    throw new RuntimeException("NebulaGraph init err !");
+                } else {
+                    log.info("NebulaGraph init Success ！");
+                }
+                return pool;
+            }
+
+
+            @Bean
+            @Scope(scopeName = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+            public Session session (NebulaPool nebulaPool){
+                try {
+                    Session session = nebulaPool.getSession("root", "123456", false);
+                    session.execute(NebulaConstant.USE+"NebulaTest"+NebulaConstant.SEMICOLON);
+                    return session;
+                } catch (Exception e) {
+                    log.error("get nebula session err , {} ", e.toString());
+                }
+                return null;
+        }
+    }
+
+
+    public void OpenNebula(MultipartFile f) throws InterruptedException, IOException {
+
+        //报存上传的文件
+        if (f.isEmpty()) {
+//            return "false";
+        }
+        String fileName = f.getOriginalFilename();
+        File dest = new File(new File("C:/Users/86152/Desktop/my/zj/impoter").getAbsolutePath()+ "/" + fileName);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            f.transferTo(dest); // 保存文件
+          //  return "true";
+        } catch (Exception e) {
+            e.printStackTrace();
+           // return "false";
+        }
+
+        //修改配置文件
+        ChangeText changeText = new ChangeText();
+        changeText.change(fileName);
+
+
+
+//        String BasefilePath = "E:\\desk\\nebula-importer\\nebula-importer\\example.yaml";
+//        String NewAddress = "./newCsv.csv";
+//        ChangeText changetext = new ChangeText();
+//        changetext.writeFile(BasefilePath, changetext.readFileContent(BasefilePath,NewAddress));
+
+        //控制台调用importer
+        System.out.println(System.getProperty("GBK"));//显示当前语言编码
+        Process p;//命令进程创建
+//        String fname=f.getName();
+        String[] cmd = new String[]{"cmd","/C","nebula-importer-windows-amd64-v3.1.0.exe","--config","C:/Users/86152/Desktop/my/zj/impoter/example.yaml"};//命令，按空格分割，windows命令行前两个cmd，/C一定要加
+        String[] cmd2 = new String[]{"cmd","/C","dir"};
+        String cmd3 = String.format("nebula-importer-windows-amd64-v3.1.0.exe --config C:/Users/86152/Desktop/my/zj/impoter/example.yaml");
+//        cmd[0] = "cd";
+//        cmd[1] = "nebula-docker-compose/";
+        try {
+            //执行命令，运行helloWord.class文件。
+            p = Runtime.getRuntime().exec(cmd,null,new File("C:/Users/86152/Desktop/my/zj/")) ;//后面还有俩参数，第二个一般是null，第三个是命令的文件目录，默认是java所在工程目录
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            //把命令输出打印
+            while((line = br.readLine()) != null)
+                System.out.println(line);
+            br.close();//输入流关闭，不关会进程错误，但我自己测试了关不关都没错
+            int exitVal = p.waitFor(); //获取进程最后返回状态
+            System.out.println("Process exitValue: " + exitVal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*在节点上增加注解*/
+//    @Data
+//    @ClassAutoMapping("modelandclass")
+//    public class ModelAndClass implements Serializable {
+//
+//        /**
+//         * id vid
+//         */
+//        @FieldAutoMapping(method = "getId", type = "Long")
+//        private Long id;
+//
+//        /**
+//         * 父级ID
+//         */
+//        @FieldAutoMapping(method = "getPid", type = "Long")
+//        private Long pid;
+//        /**
+//         * 名称
+//         */
+//        @FieldAutoMapping(method = "getName", type = "String")
+//        private String name;
+//
+//        /**
+//         * 描述
+//         */
+//        @FieldAutoMapping(method = "getDescription", type = "String")
+//        private String description;
+//
+//    }
+//    /*增加注解并继承边的通用属性*/
+//    @Data
+//    @AllArgsConstructor
+//    @NoArgsConstructor
+//    @ClassAutoMapping("project_attributeandrelationship")
+//    public class ProjectAttributeAndRelationship extends Edge implements Serializable {
+//
+//        @FieldAutoMapping(method = "getStartId",type = "Long")
+//        private Long startId;
+//
+//        @FieldAutoMapping(method = "getEndId",type = "Long")
+//        private Long endId;
+//
+//    }
+
+//    private static void printResult(ResultSet resultSet) throws UnsupportedEncodingException {
+//        List<String> colNames = resultSet.keys();
+//        for (String name : colNames) {
+//            System.out.printf("%15s |", name);
+//        }
+//        System.out.println();
+//        for (int i = 0; i < resultSet.rowsSize(); i++) {
+//            ResultSet.Record record = resultSet.rowValues(i);
+//            for (ValueWrapper value : record.values()) {
+//                if (value.isLong()) {
+//                    System.out.printf("%15s |", value.asLong());
+//                }
+//                if (value.isBoolean()) {
+//                    System.out.printf("%15s |", value.asBoolean());
+//                }
+//                if (value.isDouble()) {
+//                    System.out.printf("%15s |", value.asDouble());
+//                }
+//                if (value.isString()) {
+//                    System.out.printf("%15s |", value.asString());
+//                }
+//                if (value.isTime()) {
+//                    System.out.printf("%15s |", value.asTime());
+//                }
+//                if (value.isDate()) {
+//                    System.out.printf("%15s |", value.asDate());
+//                }
+//                if (value.isDateTime()) {
+//                    System.out.printf("%15s |", value.asDateTime());
+//                }
+//                if (value.isVertex()) {
+//                    System.out.printf("%15s |", value.asNode());
+//                }
+//                if (value.isEdge()) {
+//                    System.out.printf("%15s |", value.asRelationship());
+//                }
+//                if (value.isPath()) {
+//                    System.out.printf("%15s |", value.asPath());
+//                }
+//                if (value.isList()) {
+//                    System.out.printf("%15s |", value.asList());
+//                }
+//                if (value.isSet()) {
+//                    System.out.printf("%15s |", value.asSet());
+//                }
+//                if (value.isMap()) {
+//                    System.out.printf("%15s |", value.asMap());
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+
+
+
+
+
+}
